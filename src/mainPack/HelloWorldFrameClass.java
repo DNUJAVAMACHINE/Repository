000d8b6b2a30297dd89491cecfa5563b6a9229d7
DNUJAVAMACHINE <go-get@yandex.ru>
2012-03-18 00:23:06 +0300

    123

--- src/mainPack/HelloWorldFrameClass.java
+++ src/mainPack/HelloWorldFrameClass.java
@@ -3,16 +3,16 @@ package mainPack;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class HelloWorldFrameClass extends JFrame {
+public class HelloWorldFrameClass extends JFrame
+{
 
 	private static final long serialVersionUID = 1L;
-	
-	
-	public HelloWorldFrameClass() {
-		JLabel jlbHelloWorld = new JLabel("Hello World");
-		add(jlbHelloWorld);
-		this.setSize(100, 100);
-		// pack();
-		setVisible(true);
+
+	public HelloWorldFrameClass()
+	{
+			JLabel jlbHelloWorld = new JLabel("Hello World");
+			add(jlbHelloWorld);
+			this.setSize(100, 100);
+			setVisible(true);
 	}
 }
