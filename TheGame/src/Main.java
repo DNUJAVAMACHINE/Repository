3d23f6b6ecdda8fd21ef49007cbe3e4aa80f254c
Юрец <Ksayker@mail.ru>
2012-03-29 00:14:05 +0300

    Юрец
    Главный класс, вызывает конструктор главной формы, специально расположен
    в корне.

--- TheGame/src/Main.java
+++ TheGame/src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.SwingUtilities;
+
+import VisualComponents.MainForm;
+
+
 
 public class Main 
 {
-
+	public static void main(String arg[])
+	{
+		SwingUtilities.invokeLater(
+				new Runnable() 
+				{
+					public void run() 
+					{
+						new MainForm();
+					}
+				}
+				);
+	}
 }
