a681165e80847b8d16ea1e85a39325f465db5a5e
Юрец <Ksayker@mail.ru>
2012-03-29 00:15:07 +0300

    Юрец
    Прорисовка и обработка событий связаных с игровым полем.

---
+++ TheGame/src/VisualComponents/ImagePanel.java
@@ -0,0 +1,13 @@
+package VisualComponents;
+
+import javax.swing.JPanel;
+/**
+ * Прорисовка графич компонент
+ * Отловка ходов игрока 
+ * @author Юрий
+ *
+ */
+public class ImagePanel extends JPanel
+{
+
+}
