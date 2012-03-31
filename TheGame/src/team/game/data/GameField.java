55ebf9dc36e96a20e4437022ebe49420f455b654
Виталя <@COMP>
2012-03-31 18:40:07 +0300

    Убрал try-и

--- TheGame/src/team/game/data/GameField.java
+++ TheGame/src/team/game/data/GameField.java
@@ -9,14 +9,13 @@ public class GameField {
 	
 	public GameField(int countX, int countY) {
 		this.countX = countX;
-		this.countY = countY;
-		
+		this.countY = countY;		
 		for (int i = 0; i < countX; ++i)
 			for (int j = 0; j < countY; ++j) 
 				cells[i][j] = new Cell();			
 	}
 	
-	public Cell getCell(int i, int j) {
-		return cells[i][j];
-	}
+	public Cell getCell(int i, int j) 	{return cells[i][j];}
+	public int getCountX()				{return countX;}
+	public int getCountY()				{return countY;}
 }
--- TheGame/src/team/game/logic/GameProcessor.java
+++ TheGame/src/team/game/logic/GameProcessor.java
@@ -23,6 +23,7 @@ public class GameProcessor {
 		Cell 		Head;
 		List<Cell> 	Branches ;
 		List<Point> BranchesXY;
+		int countX, countY;
 		Tree(LocalPlayer pl, Point xy)
 		{
 			player		= pl;
@@ -32,77 +33,26 @@ public class GameProcessor {
 			Head 		= game.Field.getCell(RootXY.x,RootXY.y);
 			Branches	= new ArrayList<Cell>();
 			BranchesXY 	= new ArrayList<Point>();
-
-			try{
-				if(game.Field.getCell(RootXY.x-1,RootXY.y-1).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x-1,RootXY.y-1));
-					Point tmp = new Point();
-					tmp.x= RootXY.x-1;
-					tmp.y= RootXY.y-1;
-					BranchesXY.add(tmp);
-				}
-			
-				if(game.Field.getCell(RootXY.x,RootXY.y-1).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x,RootXY.y-1));
-					Point tmp = new Point();
-					tmp.x= RootXY.x;
-					tmp.y= RootXY.y-1;
-					BranchesXY.add(tmp);
-				}
-			
-				if(game.Field.getCell(RootXY.x+1,RootXY.y-1).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x+11,RootXY.y-1));
-					Point tmp = new Point();
-					tmp.x= RootXY.x+1;
-					tmp.y= RootXY.y-1;
-					BranchesXY.add(tmp);
-				}
-				
-				if(game.Field.getCell(RootXY.x-1,RootXY.y).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x-1,RootXY.y));
-					Point tmp = new Point();
-					tmp.x= RootXY.x-1;
-					tmp.y= RootXY.y;
-					BranchesXY.add(tmp);
-				}
-			
-				if(game.Field.getCell(RootXY.x+1,RootXY.y).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x+1,RootXY.y));
-					Point tmp = new Point();
-					tmp.x= RootXY.x+1;
-					tmp.y= RootXY.y;
-					BranchesXY.add(tmp);
-				}
-			
-				if(game.Field.getCell(RootXY.x-1,RootXY.y+1).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x-1,RootXY.y+1));
-					Point tmp = new Point();
-					tmp.x= RootXY.x-1;
-					tmp.y= RootXY.y+1;
-					BranchesXY.add(tmp);
-				}
-			
-				if(game.Field.getCell(RootXY.x,RootXY.y+1).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x,RootXY.y+1));
-					Point tmp = new Point();
-					tmp.x= RootXY.x;
-					tmp.y= RootXY.y+1;
-					BranchesXY.add(tmp);
-				}
-
-				if(game.Field.getCell(RootXY.x+1,RootXY.y+1).getOwner()== player){
-					Branches.add(game.Field.getCell(RootXY.x+1,RootXY.y+1));
-					Point tmp = new Point();
-					tmp.x= RootXY.x+1;
-					tmp.y= RootXY.y+1;
-					BranchesXY.add(tmp);
+			//----------------------------------------------------------
+			countX=game.Field.getCountX();
+			countY=game.Field.getCountY();
+			for(int i = RootXY.x-1; i<= RootXY.x+1; ++i ){
+				for(int j = RootXY.y-1; j<=RootXY.y+1; ++j ){
+					if((i>=0 && i< countX && j>=0 && j< countY && 
+							(i!=RootXY.x || j!= RootXY.y))&&
+							(game.Field.getCell(i,j).getOwner()== player)){
+						Branches.add(game.Field.getCell(i,j));
+						Point tmp 	= new Point();
+						tmp.x		= i;
+						tmp.y		= j;
+						BranchesXY.add(tmp);
+					}
 				}
 			}
-			catch (Throwable e) {
-				JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
-				e.printStackTrace();
-			}
+			//----------------------------------------------------------
 		}
+			
+			
 		void searhLife(){
 			//boolean isLife=false;
 			for(int i=0; i< BranchesXY.size(); ++i)
