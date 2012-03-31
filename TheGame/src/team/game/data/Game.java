46b460c6233514ab193f60e4c0198ccb22387b5f
AWESOME <AWESOME@AWESOME55>
2012-03-31 20:37:40 +0300

    Singleton Game

--- TheGame/src/team/game/data/Game.java
+++ TheGame/src/team/game/data/Game.java
@@ -4,12 +4,27 @@ import team.game.logic.GameProcessor;
 
 /**
  * 
- * @author Виталя
+ * @author Виталя & AWESOME
  *
  */
 public class Game {
 	public GameField 		Field;
 	public GameProcessor 	Processor;
 	public LocalPlayer[]	players;
-	public LocalPlayer		YouSelf;
+	public LocalPlayer		current;
+	
+	private static Game game = new Game();
+	
+	private Game() {
+		players = new LocalPlayer[4];
+		for (int i = 0; i < 4; ++i)
+			players[i] = null;
+		
+		Field = new GameField(20, 20);
+		Processor = new GameProcessor(this);
+	}
+	
+	public static Game getInstance() {
+		return game;
+	}
 }
