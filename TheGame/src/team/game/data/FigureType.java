e1447765c49167bba24b3c3a800a3a7e30f78565
AWESOME <AWESOME@AWESOME55>
2012-04-01 19:30:40 +0300

    FigureType

---
+++ TheGame/src/team/game/data/FigureType.java
@@ -0,0 +1,8 @@
+package team.game.data;
+
+public class FigureType {
+	public static final int SQUARE = 1;
+	public static final int STRIPS = 2;
+	public static final int CROSS  = 3;
+	public static final int CIRCLE = 4;
+}
