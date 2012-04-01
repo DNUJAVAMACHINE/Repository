ee203d4909a0a8349a65b685cf06e9a32861612c
Виталя <@COMP>
2012-04-01 19:18:36 +0300

    добави FigureTipe

--- TheGame/src/team/game/data/AbstractPlayer.java
+++ TheGame/src/team/game/data/AbstractPlayer.java
@@ -5,6 +5,7 @@ package team.game.data;
  *
  */
 public abstract class AbstractPlayer {
-	public String name;
+	public String 	name;
+	public int 		figureTipe;
 	public abstract void action(int x, int y);
 }
--- TheGame/src/team/game/data/LocalPlayer.java
+++ TheGame/src/team/game/data/LocalPlayer.java
@@ -6,8 +6,9 @@ package team.game.data;
  */
 public class LocalPlayer extends AbstractPlayer {
 	
-	public LocalPlayer(String name) {
-		this.name = name;
+	public LocalPlayer(String name, int figureTipe) {
+		this.name 		= name;
+		this.figureTipe	= figureTipe;
 	}
 	
 	public void action(int x, int y) {
