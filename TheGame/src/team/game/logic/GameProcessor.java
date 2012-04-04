43e310e6edf904009cd17297a582e9dbbecadeeb
Виталя <@COMP>
2012-04-04 21:42:46 +0300

    sdfghjklfghjkl

--- TheGame/src/team/game/logic/GameProcessor.java
+++ TheGame/src/team/game/logic/GameProcessor.java
@@ -22,23 +22,29 @@ public class GameProcessor {
 		List<Cell> 	Branches ;
 		List<Point> BranchesXY;
 		int countX, countY;
-		Tree(LocalPlayer pl, Point xy)
+		List<Point> MemoryBranches;
+		Tree(LocalPlayer pl, Point xy, List<Point> Memory)
 		{
-			player		= pl;
-			RootXY 		= new Point();
-			RootXY 		= xy;
-			Head 		= new Cell();
-			Head 		= game.Field.getCell(RootXY.x,RootXY.y);
-			Branches	= new ArrayList<Cell>();
-			BranchesXY 	= new ArrayList<Point>();
+			player			= pl;
+			RootXY 			= new Point();
+			RootXY 			= xy;
+			Head 			= new Cell();
+			Head 			= game.Field.getCell(RootXY.x,RootXY.y);
+			Branches		= new ArrayList<Cell>();
+			BranchesXY 		= new ArrayList<Point>();
+			MemoryBranches	= Memory;
 			//----------------------------------------------------------
 			countX=game.Field.getCountX();
 			countY=game.Field.getCountY();
 			for(int i = RootXY.x-1; i<= RootXY.x+1; ++i ){
 				for(int j = RootXY.y-1; j<=RootXY.y+1; ++j ){
+					Point tmpPoint = new Point();
+					tmpPoint.x=i;
+					tmpPoint.y=j;
 					if((i>=0 && i< countX && j>=0 && j< countY && 
 							(i!=RootXY.x || j!= RootXY.y))&&
-							(game.Field.getCell(i,j).getOwner()== player)){
+							(game.Field.getCell(i,j).getOwner()== player)
+							&&(! MemoryBranches.contains(tmpPoint))){
 						Branches.add(game.Field.getCell(i,j));
 						Point tmp 	= new Point();
 						tmp.x		= i;
@@ -54,25 +60,33 @@ public class GameProcessor {
 			if(BranchesXY.size()>0)
 				for(int i=0; i< BranchesXY.size(); ++i)
 				{
+					MemoryBranches.add(RootXY);
 					if(Branches.get(i).getActor()==null){
 						res = true;
 						break;
 					}
 					else{
-						Tree tmp=new Tree(player, BranchesXY.get(i));
+						Tree tmp=new Tree(player, BranchesXY.get(i), MemoryBranches);
 						res= tmp.searhLife();
-					}					
+						if(res)
+							break;
+					}
+					
 				}
 			return res;
 		}
 	}
 	public GameProcessor( Game game) {this.game = game;}
 	public boolean ProcessStep(LocalPlayer player, Point xy){
-		Tree TRcell= new Tree(player, xy);
-		if(TRcell.searhLife()&& game.Field.getCell(xy.x, xy.y).getActor()==null && game.Field.getCell(xy.x, xy.y).getOwner() != player){
+		if(game.Field.getCell(xy.x, xy.y).getOwner()==player)
+			return false;
+		Tree TRcell= new Tree(player, xy, new ArrayList<Point>());
+		if(TRcell.searhLife()&& game.Field.getCell(xy.x, xy.y).getActor()==null 
+				&& game.Field.getCell(xy.x, xy.y).getOwner() != player &&(player != game.Field.getCell(xy.x, xy.y).getOwner())){
 			game.Field.getCell(xy.x, xy.y).setActor(game.Field.getCell(xy.x, xy.y).getOwner());
 			game.Field.getCell(xy.x, xy.y).setOwner(player);
 			return true;
+		//&&(player == game.Field.getCell(xy.x, xy.y).getOwner())
 		}
 		else
 			return false;
@@ -92,7 +106,7 @@ public class GameProcessor {
 					Point tmp	= new Point();
 					tmp.x		= i;
 					tmp.y		= j;
-					Tree tree = new Tree(pl,tmp);
+					Tree tree = new Tree(pl,tmp , new  ArrayList<Point>());
 					if(tree.searhLife()){
 						res = true;
 						break;
