fd74f47c9351f8ac81a6fcc9ae86c5b0dcb83f8f
AWESOME <AWESOME@AWESOME55>
2012-04-08 14:42:24 +0300

    add LanPlayer

--- TheGame/src/team/game/data/AbstractPlayer.java
+++ TheGame/src/team/game/data/AbstractPlayer.java
@@ -5,7 +5,16 @@ package team.game.data;
  *
  */
 public abstract class AbstractPlayer {
-	public String 	name;
-	public int 		figureType;
-	public abstract void action(int x, int y);
+	public String name;
+	public int    figureType;
+
+	public String getTypeToString()	{
+		switch (figureType) {
+			case FigureType.BLUE_SQUARE:  return "синий квадрат";
+			case FigureType.GREEN_CIRCLE: return "зелёный круг";
+			case FigureType.RED_STRIPS:   return "красные полосы";
+			case FigureType.YELLOW_CROSS: return "жёлтый крестик";
+			default:                      return "";
+		}
+	}
 }
--- TheGame/src/team/game/data/LanPlayer.java
+++ TheGame/src/team/game/data/LanPlayer.java
@@ -4,9 +4,9 @@ package team.game.data;
  * @author AWESOME
  *
  */
-public class LocalPlayer extends AbstractPlayer {
-	
-	public LocalPlayer(String name, int figureType) {
+public class LanPlayer extends AbstractPlayer {
+
+	public LanPlayer(String name, int figureType) {
 		this.name 		= name;
 		this.figureType	= figureType;
 	}
